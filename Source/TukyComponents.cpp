436de4ebce85c1193eb2e27061f39fd8a7b7d96f
DanielGranero <danigraneroaudio@gmail.com>
2024-11-25 09:55:16 +0100

    Added a rotary slider as component

--- Source/TukyComponents.cpp
+++ Source/TukyComponents.cpp
@@ -9,3 +9,72 @@
 */
 
 #include "TukyComponents.h"
+#include "TukyColors.h"
+
+void TukyUI::Components::LookAndFeel::drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
+    float sliderPosProportional, float rotaryStartAngle,
+    float rotaryEndAngle, juce::Slider& slider)
+{
+    using namespace juce;
+
+    auto bounds = Rectangle<float>(x, y, width, height);
+
+    g.setColour(Colors::background);
+    g.fillEllipse(bounds);
+
+    g.setColour(Colors::blue);
+    g.drawEllipse(bounds, 2.f);
+
+    auto center = bounds.getCentre();
+    Path p;
+
+    Rectangle<float> r;
+    r.setLeft(center.getX() - 4);
+    r.setRight(center.getX() + 4);
+    r.setTop(bounds.getY() + 6);
+    r.setBottom(bounds.getY() + 14);
+
+    p.addEllipse(r);
+
+    jassert(rotaryStartAngle < rotaryEndAngle);
+
+    auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+    p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+
+    g.fillPath(p);
+}
+
+void TukyUI::Components::TukyRotarySlider::paint(juce::Graphics& g) {
+    using namespace juce;
+    auto startAng = degreesToRadians(180.f + 45.f);
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
+
+    auto range = getRange();
+    auto sliderBounds = getSliderBounds();
+
+    getLookAndFeel().drawRotarySlider(g,
+        sliderBounds.getX(),
+        sliderBounds.getY(),
+        sliderBounds.getWidth(),
+        sliderBounds.getHeight(),
+        jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0),
+        startAng,
+        endAng,
+        *this);
+}
+
+juce::Rectangle<int> TukyUI::Components::TukyRotarySlider::getSliderBounds() const {
+
+    auto bounds = getLocalBounds();
+
+    auto size = juce::jmin(bounds.getWidth(), bounds.getHeight());
+
+    size -= getTextHeight() * 2;
+
+    juce::Rectangle<int> r;
+    r.setSize(size, size);
+    r.setCentre(bounds.getCentreX(), 0);
+    r.setY(2);
+
+    return r;
+}
--- Source/TukyComponents.h
+++ Source/TukyComponents.h
@@ -7,5 +7,42 @@
 
   ==============================================================================
 */
-
+#include <JuceHeader.h>
 #pragma once
+
+namespace TukyUI {
+    namespace Components {
+        struct LookAndFeel : juce::LookAndFeel_V4
+        {
+            void drawRotarySlider(juce::Graphics& g, int x, int y, int width, int height,
+                float sliderPosProportional, float rotaryStartAngle,
+                float rotaryEndAngle, juce::Slider& slider) override;
+        };
+
+        struct TukyRotarySlider : juce::Slider
+        {
+            TukyRotarySlider(juce::RangedAudioParameter& rap, const juce::String& unitSuffix) :
+                juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,
+                    juce::Slider::TextEntryBoxPosition::NoTextBox),
+                param(&rap),
+                suffix(unitSuffix)
+            {
+                setLookAndFeel(&lnf);
+            }
+
+            ~TukyRotarySlider() {
+                setLookAndFeel(nullptr);
+            }
+
+            void paint(juce::Graphics& g) override;
+            juce::Rectangle<int> getSliderBounds() const;
+            int getTextHeight() const { return 14; }
+            juce::String getDisplayString() const;
+        private:
+            LookAndFeel lnf;
+
+            juce::RangedAudioParameter* param;
+            juce::String suffix;
+        };
+    }
+}
